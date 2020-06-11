1c949887b0ac4b507afc49585ba5351ae31ee3ca
Jerome Brongniart <jerome.brongniart@exense.ch>
2020-06-11 11:15:08 +0200

    ZU-90: Setting the FileWatcher thread as a deamon

--- exense-commons/src/main/java/ch/exense/commons/io/FileWatchService.java
+++ exense-commons/src/main/java/ch/exense/commons/io/FileWatchService.java
@@ -27,8 +27,6 @@ import java.util.Map.Entry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.exense.commons.io.FileHelper;
-
 public class FileWatchService extends Thread implements Closeable {
 	
 	private static final Logger logger = LoggerFactory.getLogger(FileWatchService.class);
@@ -39,6 +37,8 @@ public class FileWatchService extends Thread implements Closeable {
 	
 	public FileWatchService() {
 		super();
+		
+		setDaemon(true);
 		
 		start();
 	}
