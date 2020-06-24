e83436c3ab71831e39301c4acf717b2f791f12a3
Dorian Cransac <reachme@doriancransac.info>
2020-06-24 15:31:03 +0200

    Ensuring compa

---
+++ exense-core-mongo/src/main/java/ch/exense/commons/core/accessors/mongo/ObjectWrapper.java
@@ -0,0 +1,29 @@
+package ch.exense.commons.core.accessors.mongo;
+
+public class ObjectWrapper {
+	
+	private String name;
+	private Object object;
+
+	public ObjectWrapper() {
+	}
+	
+	public ObjectWrapper(String name, Object object) {
+		this.name = name;
+		this.object = object;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Object getObject() {
+		return object;
+	}
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+}
