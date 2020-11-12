670b163bf66daf1a504917f10bd2dec0e8a29524
Jerome Brongniart <jerome.brongniart@exense.ch>
2020-11-12 15:54:44 +0100

    SED-549: first version of the plugins

---
+++ exense-core-server/src/test/java/ch/exense/commons/core/access/authentication/LdaptiveLdapAuthenticatorTest.java
@@ -0,0 +1,17 @@
+package ch.exense.commons.core.access.authentication;
+
+import ch.commons.auth.Credentials;
+import junit.framework.TestCase;
+
+public class LdaptiveLdapAuthenticatorTest extends TestCase {
+
+    public void test() throws Exception {
+        LdaptiveLdapAuthenticator auth = new LdaptiveLdapAuthenticator();
+
+        Credentials cred = new Credentials();
+        cred.setUsername("raoul");
+        cred.setPassword("test");
+        assert auth.authenticate(cred);
+    }
+
+}
