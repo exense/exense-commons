363765e9997f42401b663d0404c9935e151ff2a2
Jerome Brongniart <jerome.brongniart@exense.ch>
2022-06-08 16:08:54 +0200

    SED-1006: Fixing tests

--- exense-core-web/src/main/java/ch/exense/commons/core/web/container/DemoPublicServices.java
+++ exense-core-web/src/main/java/ch/exense/commons/core/web/container/DemoPublicServices.java
@@ -15,10 +15,9 @@
  ******************************************************************************/
 package ch.exense.commons.core.web.container;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
