d7459260a9366fc00672ff9bb15395bced813c20
Jérôme Comte <jerome.comte@exense.ch>
2023-01-19 09:59:53 +0100

    SED-1657 Extending ManagedProcess to set execution directory

--- exense-commons-parent/exense-basic-commons/src/main/java/ch/exense/commons/processes/ManagedProcess.java
+++ exense-commons-parent/exense-basic-commons/src/main/java/ch/exense/commons/processes/ManagedProcess.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2021 exense GmbH
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -15,11 +15,11 @@
  ******************************************************************************/
 package ch.exense.commons.processes;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import ch.exense.commons.io.FileHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.security.InvalidParameterException;
@@ -31,231 +31,264 @@ import java.util.concurrent.TimeoutException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import ch.exense.commons.io.FileHelper;
-
 /**
  * This class is a wrapper of {@link Process} which offers additional services
  * like better handling of standard output and standard error output.
- * 
  */
 public class ManagedProcess implements Closeable {
 
-	private static final Logger logger = LoggerFactory.getLogger(ManagedProcess.class);
+    private static final Logger logger = LoggerFactory.getLogger(ManagedProcess.class);
 
-	private static final String DEFAULT_PROCESS_NAME = "ManagedProcess";
+    private static final String DEFAULT_PROCESS_NAME = "ManagedProcess";
 
-	private final ProcessBuilder builder;
-	private final String id;
-	private final File executionDirectory;
-	
-	private boolean redirectOuput;
+    private final ProcessBuilder builder;
+    private final String id;
+    private final File executionDirectory;
+    private final File tempLogDirectory;
 
-	private Process process;
-	private File processOutputLog;
-	private File processErrorLog;
-	
-	/**
-	 * @param command the command to be executed (the program and its arguments)
-	 *                formatted as string
-	 * @throws ManagedProcessException
-	 */
-	public ManagedProcess(String command) throws ManagedProcessException {
-		this(DEFAULT_PROCESS_NAME, tokenize(command));
-	}
+    private final boolean redirectOuput;
 
-	/**
-	 * @param name    a string describing the process. This is used to prefix the ID
-	 *                which uniquely identifies the process instance
-	 * @param command the command to be executed (the program and its arguments)
-	 *                formatted as string
-	 * @throws ManagedProcessException
-	 */
-	public ManagedProcess(String name, String command) throws ManagedProcessException {
-		this(name, tokenize(command));
-	}
+    private Process process;
+    private File processOutputLog;
+    private File processErrorLog;
 
-	/**
-	 * @param commands the list containing the program and its arguments
-	 * @throws ManagedProcessException
-	 */
-	public ManagedProcess(List<String> commands) throws ManagedProcessException {
-		this(DEFAULT_PROCESS_NAME, commands, null, true);
-	}
+    /**
+     * @param command the command to be executed (the program and its arguments)
+     *                formatted as string
+     */
+    public ManagedProcess(String command) throws ManagedProcessException {
+        this(DEFAULT_PROCESS_NAME, tokenize(command));
+    }
 
-	/**
-	 * @param name    a string describing the process. This is used to prefix the ID
-	 *                which uniquely identifies the process instance
-	 * @param commands the list containing the program and its arguments
-	 * @throws ManagedProcessException
-	 */
-	public ManagedProcess(String name, List<String> commands) throws ManagedProcessException {
-		this(name, commands, null, true);
-	}
+    /**
+     * @param name    a string describing the process. This is used to prefix the ID
+     *                which uniquely identifies the process instance
+     * @param command the command to be executed (the program and its arguments)
+     *                formatted as string
+     */
+    public ManagedProcess(String name, String command) throws ManagedProcessException {
+        this(name, tokenize(command));
+    }
 
-	/**
-	 * @param commands      the list containing the program and its arguments
-	 * @param logDirectory the directory where the logs (stdout and stderr) of the
-	 *                     process will be written to
-	 * @throws ManagedProcessException
-	 */
-	public ManagedProcess(List<String> commands, File logDirectory) throws ManagedProcessException {
-		this(DEFAULT_PROCESS_NAME, commands, logDirectory,true);
-	}
+    /**
+     * @param commands the list containing the program and its arguments
+     */
+    public ManagedProcess(List<String> commands) throws ManagedProcessException {
+        this(DEFAULT_PROCESS_NAME, commands, null, true);
+    }
 
-	/**
-	 * @param name         a string describing the process. This is used to prefix
-	 *                     the ID wich uniquely identifies the process instance
-	 * @param commands      the list containing the program and its arguments
-	 * @param logDirectory the directory where the logs (stdout and stderr) of the
-	 *                     process will be written to
-	 * @throws ManagedProcessException
-	 */
-	public ManagedProcess(String name, List<String> commands, File logDirectory, boolean redirectOuput) throws ManagedProcessException {
-		super();
+    /**
+     * @param name     a string describing the process. This is used to prefix the ID
+     *                 which uniquely identifies the process instance
+     * @param commands the list containing the program and its arguments
+     */
+    public ManagedProcess(String name, List<String> commands) throws ManagedProcessException {
+        this(name, commands, null, true);
+    }
 
-		this.redirectOuput = redirectOuput;
-		
-		if (logDirectory == null) {
-			logDirectory = new File(".");
-		}
+    /**
+     * @param commands     the list containing the program and its arguments
+     * @param logDirectory the directory where the logs (stdout and stderr) of the
+     *                     process will be written to
+     */
+    public ManagedProcess(List<String> commands, File logDirectory) throws ManagedProcessException {
+        this(DEFAULT_PROCESS_NAME, commands, logDirectory, true);
+    }
 
-		UUID uuid = UUID.randomUUID();
-		this.id = name + "_" + uuid;
-		builder = new ProcessBuilder(commands);
+    public ManagedProcess(String name, List<String> commands, File baseLogDirectory, boolean redirectOuput) throws ManagedProcessException {
+        this(name, commands, null, baseLogDirectory, redirectOuput);
+    }
 
-		executionDirectory = new File(logDirectory.getAbsolutePath() + "/" + id);
-		if (!executionDirectory.exists()) {
-			if (!executionDirectory.mkdirs()) {
-				throw new InvalidParameterException("Unable to create log folder for process " + id
-						+ ". Please ensure that the folder " + logDirectory.getAbsolutePath() + " exists and is writable.");
-			}
-		}
-	}
+    /**
+     * @param name               a string describing the process. This is used to prefix
+     *                           the ID wich uniquely identifies the process instance
+     * @param commands           the list containing the program and its arguments
+     * @param executionDirectory the directory in which the process will be executed
+     * @param baseLogDirectory   the directory where the temporary directory containing the logs (stdout and stderr) of the
+     *                           process will be created
+     * @param redirectOuput      if the std output of the process should be redirected to a temporary file or kept in the output stream
+     */
+    public ManagedProcess(String name, List<String> commands, File executionDirectory, File baseLogDirectory, boolean redirectOuput) {
+        super();
 
-	public String getId() {
-		return id;
-	}
+        this.redirectOuput = redirectOuput;
+        this.id = name + "_" + UUID.randomUUID();
+        this.builder = new ProcessBuilder(commands);
 
-	public OutputStream getProcessOutputStream() {
-		return process.getOutputStream();
-	}
+        if (baseLogDirectory == null) {
+            baseLogDirectory = new File(".");
+        }
 
-	public InputStream getProcessInputStream() throws Exception {
-		if (redirectOuput) {
-			throw new Exception("The process InputStream could not be retrived if it has been redirected in a file");
-		}
-		return process.getInputStream();
-	}
+        this.tempLogDirectory = new File(baseLogDirectory.getAbsolutePath() + "/" + id);
 
-	public File getProcessOutputLog() {
-		return processOutputLog;
-	}
+        if (executionDirectory == null) {
+            this.executionDirectory = tempLogDirectory;
+        } else {
+            this.executionDirectory = executionDirectory;
+        }
 
-	public File getProcessErrorLog() {
-		return processErrorLog;
-	}
+        createDirectoryIfNotExisting(this.tempLogDirectory);
+        createDirectoryIfNotExisting(this.executionDirectory);
+    }
 
-	public File getExecutionDirectory() {
-		return executionDirectory;
-	}
+    private void createDirectoryIfNotExisting(File directory) {
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                throw new InvalidParameterException("Unable to create directory for process " + id
+                        + ". Please ensure that the file " + directory.getAbsolutePath() + " is writable.");
+            }
+        }
+    }
 
-	private static List<String> tokenize(String command) {
-		List<String> tokens = new ArrayList<String>();
-		Pattern regex = Pattern.compile("[^\\s\"]+|\"([^\"]*)\"");
-		Matcher regexMatcher = regex.matcher(command);
-		while (regexMatcher.find()) {
-			if (regexMatcher.group(1) != null) {
-				tokens.add(regexMatcher.group(1));
-			} else {
-				tokens.add(regexMatcher.group());
-			}
-		}
-		return tokens;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void start() throws ManagedProcessException {
-		synchronized (this) {
-			try {
-				if (process == null) {
-					logger.debug("Starting managed process " + builder.command());
-					builder.directory(executionDirectory);
+    public OutputStream getProcessOutputStream() {
+        return process.getOutputStream();
+    }
 
-					if (redirectOuput) {
-						processOutputLog = new File(executionDirectory + "/ProcessOut.log");
-						builder.redirectOutput(processOutputLog);
-					}
+    public InputStream getProcessInputStream() throws Exception {
+        if (redirectOuput) {
+            throw new Exception("The process InputStream could not be retrieved if it has been redirected in a file");
+        }
+        return process.getInputStream();
+    }
 
-					processErrorLog = new File(executionDirectory + "/ProcessError.log");
-					builder.redirectError(processErrorLog);
-					try {
-						process = builder.start();
-					} catch (IOException e) {
-						throw new ManagedProcessException("Unable to start the process " + id, e);
-					}
-					logger.debug("Started managed process " + builder.command());
-				} else {
-					throw new ManagedProcessException("Unable to start the process " + id + " twice. The process has already been started.");
-				}
-			} catch (ManagedProcessException e) {
-				throw e;
-			} catch (Throwable t) {
-				removeFolder();
-			}
-		}
-	}
+    public File getProcessOutputLog() {
+        return processOutputLog;
+    }
 
+    public File getProcessErrorLog() {
+        return processErrorLog;
+    }
 
-	/**
-	 * Causes the current thread to wait, if necessary, until the process has
-	 * terminated, or the specified waiting time elapses.
-	 * 
-	 * @param timeout the maximum time to wait in ms
-	 * @throws TimeoutException     if the process doesn't exit within the defined
-	 *                              timeout
-	 * @throws InterruptedException if the thread is interrupted while waiting for
-	 *                              the process to exit
-	 */
-	public int waitFor(long timeout) throws TimeoutException, InterruptedException {
-		boolean terminated = process.waitFor(timeout, TimeUnit.MILLISECONDS);
-		if (!terminated) {
-			throw new TimeoutException(
-					"The process " + id + " didn't exit within the defined timeout of " + timeout + "ms");
-		}
-		return process.exitValue();
-	}
+    public String getProcessOutputLogAsString() {
+        return readProcessLog(getProcessOutputLog());
+    }
 
-	public class ManagedProcessException extends Exception {
+    public String getProcessErrorLogAsString() {
+        return readProcessLog(getProcessErrorLog());
+    }
 
-		private static final long serialVersionUID = -2205566982535606557L;
+    private static String readProcessLog(File file) {
+        try {
+            return Files.readString(file.toPath(), Charset.defaultCharset());
+        } catch (IOException e) {
+            String errorMessage = "Error while reading process log file " + file.getAbsolutePath();
+            logger.error(errorMessage, e);
+            throw new RuntimeException(errorMessage, e);
+        }
+    }
 
-		public ManagedProcessException(String message, Throwable cause) {
-			super(message, cause);
-		}
+    public File getExecutionDirectory() {
+        return executionDirectory;
+    }
 
-		public ManagedProcessException(String message) {
-			super(message);
-		}
-	}
+    private static List<String> tokenize(String command) {
+        List<String> tokens = new ArrayList<>();
+        Pattern regex = Pattern.compile("[^\\s\"]+|\"([^\"]*)\"");
+        Matcher regexMatcher = regex.matcher(command);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group(1) != null) {
+                tokens.add(regexMatcher.group(1));
+            } else {
+                tokens.add(regexMatcher.group());
+            }
+        }
+        return tokens;
+    }
 
-	@Override
-	public void close() throws IOException {
+    public void start() throws ManagedProcessException {
+        synchronized (this) {
+            try {
+                if (process == null) {
+                    logger.debug("Starting managed process " + builder.command());
+                    builder.directory(executionDirectory);
+
+                    if (redirectOuput) {
+                        processOutputLog = new File(tempLogDirectory + "/ProcessOut.log");
+                        builder.redirectOutput(processOutputLog);
+                    }
+
+                    processErrorLog = new File(tempLogDirectory + "/ProcessError.log");
+                    builder.redirectError(processErrorLog);
+                    try {
+                        process = builder.start();
+                    } catch (IOException e) {
+                        throw new ManagedProcessException("Unable to start the process " + id, e);
+                    }
+                    logger.debug("Started managed process " + builder.command());
+                } else {
+                    throw new ManagedProcessException("Unable to start the process " + id + " twice. The process has already been started.");
+                }
+            } catch (ManagedProcessException e) {
+                throw e;
+            } catch (Throwable t) {
+                removeTempLogDirectory();
+            }
+        }
+    }
+
+    /**
+     * Start the process and wait until the process has terminated
+     *
+     * @param timeout the maximum time to wait in ms
+     * @return the exit value of the process
+     */
+    public int startAndWaitFor(long timeout) throws TimeoutException, InterruptedException, ManagedProcessException {
+        start();
+        return waitFor(timeout);
+    }
+
+    /**
+     * Causes the current thread to wait, if necessary, until the process has
+     * terminated, or the specified waiting time elapses.
+     *
+     * @param timeout the maximum time to wait in ms
+     * @throws TimeoutException     if the process doesn't exit within the defined
+     *                              timeout
+     * @throws InterruptedException if the thread is interrupted while waiting for
+     *                              the process to exit
+     */
+    public int waitFor(long timeout) throws TimeoutException, InterruptedException {
+        boolean terminated = process.waitFor(timeout, TimeUnit.MILLISECONDS);
+        if (!terminated) {
+            throw new TimeoutException(
+                    "The process " + id + " didn't exit within the defined timeout of " + timeout + "ms");
+        }
+        return process.exitValue();
+    }
+
+    public static class ManagedProcessException extends Exception {
+
+        private static final long serialVersionUID = -2205566982535606557L;
+
+        public ManagedProcessException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public ManagedProcessException(String message) {
+            super(message);
+        }
+    }
+
+    @Override
+    public void close() {
         if (logger.isDebugEnabled()) {
             try {
-                String errorLog = new String(Files.readAllBytes(getProcessErrorLog().toPath()), Charset.defaultCharset());
+                String errorLog = readProcessLog(getProcessErrorLog());
                 logger.debug("Error output from managed process " + id + ": " + errorLog);
                 logger.debug("End of error output from managed process " + id);
 
                 if (redirectOuput) {
-                    String stdOut = new String(Files.readAllBytes(getProcessOutputLog().toPath()), Charset.defaultCharset());
+                    String stdOut = readProcessLog(getProcessOutputLog());
                     logger.debug("Standard output from managed process " + id + ": " + stdOut);
                 } else {
                     logger.debug("Standard output from managed process " + id + " was not redirected, nothing to display.");
                 }
                 logger.debug("End of standard output from managed process " + id);
-            } catch (IOException e) {
+            } catch (Exception e) {
                 logger.error("Error while logging output of process " + id, e);
             }
         }
@@ -264,16 +297,17 @@ public class ManagedProcess implements Closeable {
             process.destroy();
             try {
                 process.waitFor();
-            } catch (InterruptedException e) {
+            } catch (InterruptedException ignored) {
             }
         }
-		removeFolder();
+        removeTempLogDirectory();
     }
 
-    private void removeFolder() {
-		try {
-			FileHelper.deleteFolder(executionDirectory);
-		} catch (Throwable t) {}
-	}
+    private void removeTempLogDirectory() {
+        try {
+            FileHelper.deleteFolder(tempLogDirectory);
+        } catch (Throwable ignored) {
+        }
+    }
 
 }
--- exense-commons-parent/exense-basic-commons/src/test/java/ch/exense/commons/processes/ManagedProcessTest.java
+++ exense-commons-parent/exense-basic-commons/src/test/java/ch/exense/commons/processes/ManagedProcessTest.java
@@ -15,63 +15,121 @@
  ******************************************************************************/
 package ch.exense.commons.processes;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import ch.exense.commons.io.FileHelper;
+import ch.exense.commons.processes.ManagedProcess.ManagedProcessException;
+import org.junit.Test;
+
+import java.io.*;
 import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
-import org.junit.Test;
-
-import ch.exense.commons.io.FileHelper;
-import ch.exense.commons.processes.ManagedProcess.ManagedProcessException;
-import junit.framework.Assert;
+import static org.junit.Assert.*;
 
 public class ManagedProcessTest  {
 
-//	@Test
+	@Test
 	public void test() throws IOException, ManagedProcessException, TimeoutException, InterruptedException {
 		try(ManagedProcess managedProcess = new ManagedProcess("java -version")) {
 			runAndTestProcess(managedProcess);
 		}
 	}
 	
-//	@Test
+	@Test
 	public void test2() throws IOException, ManagedProcessException, TimeoutException, InterruptedException {
 		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", "java -version")) {
 			runAndTestProcess(managedProcess);
-			Assert.assertTrue(managedProcess.getId().startsWith("MyJavaProcess_"));
+			assertTrue(managedProcess.getId().startsWith("MyJavaProcess_"));
 		}
 	}
 	
-//	@Test
+	@Test
 	public void test3() throws IOException, ManagedProcessException, TimeoutException, InterruptedException {
-		try(ManagedProcess managedProcess = new ManagedProcess(Arrays.asList(new String[] {"java", "-version"}))) {
+		try(ManagedProcess managedProcess = new ManagedProcess(Arrays.asList("java", "-version"))) {
 			runAndTestProcess(managedProcess);
 		}
 	}
 	
-//	@Test
+	@Test
 	public void test4() throws IOException, ManagedProcessException, TimeoutException, InterruptedException {
-		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", Arrays.asList(new String[] {"java", "-version"}))) {
+		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", Arrays.asList("java", "-version"))) {
 			runAndTestProcess(managedProcess);
-			Assert.assertTrue(managedProcess.getId().startsWith("MyJavaProcess_"));
+			assertTrue(managedProcess.getId().startsWith("MyJavaProcess_"));
 		}
 	}
 	
-//	@Test
+	@Test
 	public void test5() throws IOException, ManagedProcessException, TimeoutException, InterruptedException {
 		File tempFolder = FileHelper.createTempFolder();
-		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", Arrays.asList(new String[] {"java", "-version"}), tempFolder, true)) {
+		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", Arrays.asList("java", "-version"), tempFolder, true)) {
 			runAndTestProcess(managedProcess);
-			Assert.assertTrue(managedProcess.getId().startsWith("MyJavaProcess_"));
-			Assert.assertEquals(managedProcess.getId(), tempFolder.listFiles()[0].getName());
+			assertTrue(managedProcess.getId().startsWith("MyJavaProcess_"));
+			assertEquals(managedProcess.getId(), tempFolder.listFiles()[0].getName());
 		}
 		// Assert that the process log have been deleted
-		Assert.assertEquals(0, tempFolder.listFiles().length);
+		assertEquals(0, tempFolder.listFiles().length);
 	}
 
-//	@Test
+	@Test
+	public void testExecutionDirectory() throws IOException, ManagedProcessException, TimeoutException, InterruptedException {
+		File tempExecutionFolder = FileHelper.createTempFolder();
+
+		// Create a simple java program
+		FileWriter fileWriter = new FileWriter(tempExecutionFolder.toPath().resolve("test.java").toFile());
+		fileWriter.write("public class HelloWorld {\n" +
+				"\n" +
+				"    public static void main(String[] args) {\n" +
+				"        System.out.println(\"Hello World!\");\n" +
+				"    }\n" +
+				"\n" +
+				"}");
+		fileWriter.close();
+
+		File tempLogFolder = FileHelper.createTempFolder();
+		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", Arrays.asList("java", "test.java"), tempExecutionFolder, tempLogFolder, true)) {
+			managedProcess.startAndWaitFor(5000);
+			assertTrue(managedProcess.getProcessOutputLogAsString().startsWith("Hello World!"));
+			// Assert that the process logs have been created within the log directory
+			assertEquals(2, tempLogFolder.toPath().resolve(managedProcess.getId()).toFile().listFiles().length);
+			// Assert that the process returns the correct execution directory
+			assertEquals(tempExecutionFolder, managedProcess.getExecutionDirectory());
+		}
+		// Assert that the process log have been deleted
+		assertEquals(0, tempLogFolder.listFiles().length);
+		// Assert that the execution directory still exist
+		assertEquals(1, tempExecutionFolder.listFiles().length);
+	}
+
+	@Test
+	public void testInputOutputStream() throws Exception {
+		File tempExecutionFolder = FileHelper.createTempFolder();
+
+		// Create a simple java program that prints the content of the input stream
+		FileWriter fileWriter = new FileWriter(tempExecutionFolder.toPath().resolve("test.java").toFile());
+		fileWriter.write("public class HelloWorld {\n" +
+				"    public static void main(String[] args) throws java.io.IOException {\n" +
+				"        java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));\n" +
+				"        String name = reader.readLine();\n" +
+				"        System.out.println(name);" +
+				"    }\n" +
+				"}");
+		fileWriter.close();
+
+		File tempLogFolder = FileHelper.createTempFolder();
+		try(ManagedProcess managedProcess = new ManagedProcess("MyJavaProcess", Arrays.asList("java", "test.java"), tempExecutionFolder, tempLogFolder, false)) {
+			managedProcess.start();
+			OutputStreamWriter writer = new OutputStreamWriter(managedProcess.getProcessOutputStream());
+			writer.write("hello\n");
+			writer.close();
+			managedProcess.waitFor(1000);
+
+			InputStream processInputStream = managedProcess.getProcessInputStream();
+			BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(processInputStream));
+			String line = inputStreamReader.readLine();
+			assertTrue(line.startsWith("hello"));
+		}
+	}
+
+	@Test
 	public void testError() throws IOException, TimeoutException, InterruptedException {
 		Exception actual = null;
 		try(ManagedProcess managedProcess = new ManagedProcess("invalid cmd")) {
@@ -79,11 +137,11 @@ public class ManagedProcessTest  {
 		} catch (ManagedProcessException e) {
 			actual = e;
 		}
-		Assert.assertNotNull(actual);
-		Assert.assertTrue(actual.getMessage().startsWith("Unable to start the process "));
+		assertNotNull(actual);
+		assertTrue(actual.getMessage().startsWith("Unable to start the process "));
 	}
 	
-	//@Test
+	@Test
 	public void testMultipleStarts() throws IOException, TimeoutException, InterruptedException {
 		Exception actual = null;
 		String id = null;
@@ -94,19 +152,19 @@ public class ManagedProcessTest  {
 		} catch (ManagedProcessException e) {
 			actual = e;
 		}
-		Assert.assertNotNull(actual);
-		Assert.assertEquals("Unable to start the process "+id+ " twice. The process has already been started.", actual.getMessage());
+		assertNotNull(actual);
+		assertEquals("Unable to start the process "+id+ " twice. The process has already been started.", actual.getMessage());
 	}
 	
 	private void runAndTestProcess(ManagedProcess managedProcess)
-			throws ManagedProcessException, TimeoutException, InterruptedException, IOException {
+			throws ManagedProcessException, TimeoutException, InterruptedException {
 		managedProcess.start();
 		int exitValue = managedProcess.waitFor(1000);
-		String errOutput = new String(Files.readAllBytes(managedProcess.getProcessErrorLog().toPath()));
-		String stdOutput = new String(Files.readAllBytes(managedProcess.getProcessOutputLog().toPath()));
-		Assert.assertEquals(0, exitValue);
-		Assert.assertTrue(errOutput.startsWith("java version"));
-		Assert.assertTrue(stdOutput.startsWith(""));
+		String errOutput = managedProcess.getProcessErrorLogAsString();
+		String stdOutput = managedProcess.getProcessOutputLogAsString();
+		assertEquals(0, exitValue);
+		assertTrue(errOutput.contains("version"));
+		assertTrue(stdOutput.isEmpty());
 	}
 
 }
