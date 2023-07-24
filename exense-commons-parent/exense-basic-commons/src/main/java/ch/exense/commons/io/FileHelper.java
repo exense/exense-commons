0a0a9e3afc14a9a652b1b9144c29e97e52ea676b
Jerome Brongniart <jerome.brongniart@exense.ch>
2023-07-24 17:44:38 +0200

    SED-2329 refactoring for loop

--- exense-commons-parent/exense-basic-commons/src/main/java/ch/exense/commons/io/FileHelper.java
+++ exense-commons-parent/exense-basic-commons/src/main/java/ch/exense/commons/io/FileHelper.java
@@ -307,12 +307,12 @@ public class FileHelper {
 		File[] files = directory.listFiles();
 		byte[] buffer = new byte[8192];
 		int read = 0;
-		for (int i = 0, n = files.length; i < n; i++) {
-			if (files[i].isDirectory()) {
-				zip(files[i], base, zos);
+		for (File file : Objects.requireNonNull(files)) {
+			if (file.isDirectory()) {
+				zip(file, base, zos);
 			} else {
-				FileInputStream in = new FileInputStream(files[i]);
-				ZipEntry entry = new ZipEntry(files[i].getPath().substring(base.getPath().length() + 1).replaceAll("\\\\","/"));
+				FileInputStream in = new FileInputStream(file);
+				ZipEntry entry = new ZipEntry(file.getPath().substring(base.getPath().length() + 1).replaceAll("\\\\", "/"));
 				zos.putNextEntry(entry);
 				while (-1 != (read = in.read(buffer))) {
 					zos.write(buffer, 0, read);
